7abc2eff5f0d34de51f812560ca8c60199bbf21d
Retr0ville <xclusivesam14@gmail.com>
2022-07-23 14:17:49 +0100

    revolution in progress

--- src/components/Revolution/index.tsx
+++ src/components/Revolution/index.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 
 const Revolution = () => (
   <div>
-    <section>
-      <div>
-        <div>
-          <div>
-            <h2>
+    <section className="tw-relative lg:tw-min-h-[660px]">
+      <div className="tw-flex tw-items-center tw-justify-center md:tw-h-[448px] lg:tw-h-[660px]">
+        <div className="tw-grid xlalt:tw-grid-cols-12 tw-grid-cols-4 tw-px-4 tw-gap-x-4 tw-gap-y-8 tw-justify-center  tw-w-full">
+          <div className="tw-col-span-6">
+            <h2 className="tw-p-0 tw-text-white font-bebas tw-font-normal tw-text-[40px] tw-leading-[44px] tw-tracking-normal tw-text-center lg:tw-text-[62px] lg:tw-leading-[64px]  ">
               A Revolutionary Music Identification Tool for Creators
             </h2>
             <p className="tw-text-white tw-px-6">
@@ -17,7 +17,7 @@ const Revolution = () => (
           </div>
         </div>
       </div>
-      <div>
+      <div className="tw-flex tw-justify-end tw-w-full lg:tw-h-[660px] md:tw-absolute md:tw-top-0 md:tw-w-1/2 md:tw-h-[448px]">
         <div>
           <img src="images/cards.png" alt="" />
           <div>
