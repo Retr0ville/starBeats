c548799138985f73fe48ac749261d8f1cc3cd8c3
Retr0ville <xclusivesam14@gmail.com>
2022-07-27 14:19:03 +0100

    faq init

---
+++ src/components/FAQ/Faq.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable quotes */
+import React from "react";
+
+const Faq = () => (
+  <div>
+    <div>
+      <section>
+        qqq
+      </section>
+    </div>
+  </div>
+);
+
+export default Faq;
