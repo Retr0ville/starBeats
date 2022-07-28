54da0b6693a05cb56ec306054c73889cecdedcc5
Retr0ville <xclusivesam14@gmail.com>
2022-07-28 19:11:43 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,22 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      backgroundPosition: {
+        full: "100%",
+      },
+      backgroundImage: {
+        discover: "url('/public/images/hero-headphones.jpg')",
+      },
       colors: {
         primary: "#141414",
       },
       screens: {
-        'ssm': "480px",
-        '2md': "960px",
-        'xlalt': "1200px",
-        'sxlalt': "1348px",
-        '2xlalt': "1400px",
-        '3xlalt': "1600px",
+        ssm: "480px",
+        "2md": "960px",
+        xlalt: "1200px",
+        sxlalt: "1348px",
+        "2xlalt": "1400px",
+        "3xlalt": "1600px",
       },
     },
   },
