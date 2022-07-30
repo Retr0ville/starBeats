1c5cb3772cc5dd76664a2d0d80e442c77c665ba1
Retr0ville <xclusivesam14@gmail.com>
2022-07-30 12:32:18 +0100

    mail form fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -39,12 +39,19 @@ const Footer = () => {
               Yes! Send me personalized tips for shopping and selling on BeatStars.
             </h3>
             <div>
-              <form action="POST" className="ssm:tw-max-w-[484px] ssm:tw-flex-row ssm:tw-w-[484px] ssm:tw-items-stretch ssm:tw-p-2 ssm:tw-bg-[#282828] ssm:tw-rounded-[5px] tw-flex tw-flex-col tw-items-center ">
+              <form action="POST" className="ssm:tw-max-w-[484px] ssm:tw-flex-row 2md:tw-w-[484px] ssm:tw-items-stretch ssm:tw-p-2 ssm:tw-bg-[#282828] ssm:tw-rounded-[5px] tw-flex tw-flex-col tw-items-center ">
                 <div className="tw-text-[#707070] tw-flex tw-items-center tw-justify-start tw-w-full tw-h-16 tw-bg-[#282828] tw-rounded[5px] ssm:tw-flex-1  ssm:tw-h-12">
                   <FaEnvelope size="49" className="tw-text-[28px] tw-p-5  tw-inline-block ssm:tw-p-[8px_16px_8px_8px]" />
-                  <input onChange={(e) => { setMail(e.target.value); }} required type="email" placeholder="Enter your e-mail" className="tw-w-full tw-text-xs tw-leading-[1.5] tw-font-[700] tw-text-white tw-tracking-normal tw-bg-transparent tw-border-0 tw-border-none ssm:tw-mr-5 focus-visible:tw-outline-none" />
+                  <input
+                    onChange={(e) => { setMail(e.target.value); }}
+                    required
+                    type="email"
+                    placeholder="Enter your e-mail"
+                    className="tw-w-full tw-text-xs tw-leading-[1.5] tw-font-[700] tw-text-white
+                  placeholder:tw-text-zinc-500  tw-tracking-normal tw-bg-transparent tw-border-0 tw-border-none ssm:tw-mr-5 focus-visible:tw-outline-none"
+                  />
                 </div>
-                <button type="submit" disabled={!validMail} onClick={() => { }} className="disabled:tw-text-[#ffffff33] tw-text-white tw-w-100 tw-h-16 tw-text-[18px] tw-font-[500] tw-items-center tw-tracking-normal tw-bg-[#005ff8]  tw-border-0 tw-border-none tw-rounded-[5px] disabled:tw-bg-[#26262680] tw-cursor-auto ssm:tw-w-[112px] tw-min-w-[112px] ssm:tw-h-12 tw-mt-0 ssm:tw-text-[16px] ssm:tw-leading-[1.5]">
+                <button type="submit" disabled={!validMail} onClick={() => { }} className="disabled:tw-text-[#ffffff33] tw-text-white tw-w-full tw-h-16 tw-mt-4 tw-text-[18px] tw-font-[500] tw-items-center tw-tracking-normal tw-bg-[#005ff8]  tw-border-0 tw-border-none tw-rounded-[5px] disabled:tw-bg-[#26262680] tw-cursor-auto ssm:tw-w-[112px] ssm:tw-min-w-[112px] ssm:tw-h-12 ssm:tw-mt-0 ssm:tw-text-[16px] ssm:tw-leading-[1.5]">
                   Subscribe
                 </button>
               </form>
